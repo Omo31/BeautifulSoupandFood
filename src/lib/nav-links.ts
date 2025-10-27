c62dca850a5f270c02507c1de2a2aec1478a82a4
Omowumi Badmus <oluwagbengwumi@gmail.com>
2025-10-27 21:07:08 +0000

    please remove it if it is causing this error

--- src/lib/nav-links.ts
+++ src/lib/nav-links.ts
@@ -30,7 +30,6 @@ export const adminNavItems = [
   { href: '/inventory', label: 'Inventory', icon: Package },
   { href: '/flyer-generator', label: 'Flyer Generator', icon: Megaphone },
   { href: '/video-generator', label: 'Video Generator', icon: Video },
-  { href: '/analytics', label: 'Analytics', icon: BarChart },
   { href: '/accounting', label: 'Accounting', icon: Book },
   { href: '/settings/homepage', label: 'Settings', icon: Settings },
 ];
